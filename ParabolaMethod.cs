94de4257389f15d533e1829bda0799352666f2c2
MagicDEveloper45 <56794850+MagicDEveloper45@users.noreply.github.com>
2021-03-27 18:36:35 +0300

    Commit 4
    ParabolaMethod upd

--- ParabolaMethod.cs
+++ ParabolaMethod.cs
@@ -6,15 +6,20 @@ namespace MO_LAB_1
 {
     class ParabolaMethod
     {
-        public double u1, u2, u3;
+        private double u1, u2, u3, a, b;
+        public double e, uRes, jRes;
         private double w, temp1, temp2, delta = 0.01;
+        public int counter;
 
 
-        public ParabolaMethod(double u1, double u2, double u3)
+        public ParabolaMethod(double u1, double u2, double u3, double e, double a, double b)
         {
             this.u1 = u1;
             this.u2 = u2;
             this.u3 = u3;
+            this.e = e;
+            this.a = a;
+            this.b = b;
         }
 
         private bool convexityСheck()
@@ -39,66 +44,72 @@ namespace MO_LAB_1
             }           
 
             w = u2 + (Math.Pow(u3 - u2, 2) * temp1 - Math.Pow(u2 - u1, 2) * temp2) / (2 * ((u3 - u2) * temp1 + (u2 - u1) * temp2));
+            counter = 0;
 
-            if (w < u2)
+            while (u3 - u1 > e)
             {
-                if (func(w) < func(u2))
+
+                if (w < u2)
                 {
-                    u2 = w;
-                    u3 = u2;
-                }
-                else if (func(w) > func(u2))
-                {
-                    u1 = w;
-                }
-                else if (func(w) == func(u2))
-                {
-                    if (func(u1) > func(u2))
+                    if (func(w) < func(u2))
                     {
                         u2 = w;
                         u3 = u2;
                     }
-                    else if (func(u2) > func(u3))
+                    else if (func(w) > func(u2))
                     {
                         u1 = w;
                     }
+                    else if (func(w) == func(u2))
+                    {
+                        if (func(u1) > func(u2))
+                        {
+                            u2 = w;
+                            u3 = u2;
+                        }
+                        else if (func(u2) > func(u3))
+                        {
+                            u1 = w;
+                        }
+                    }
                 }
-            }
 
-            if (w > u2)
-            {
-                if (func(w) < func(u2))
+                if (w > u2)
                 {
-                    u1 = u2;
-                    u2 = w;
-                }
-                else if (func(w) > func(u2))
-                {
-                    u3 = w;
-                }
-                else if (func(w) == func(u2))
-                {
-                    if (func(u3) > func(u2))
+                    if (func(w) < func(u2))
                     {
                         u1 = u2;
                         u2 = w;
                     }
-                    else if (func(u1) > func(u2))
+                    else if (func(w) > func(u2))
                     {
                         u3 = w;
                     }
+                    else if (func(w) == func(u2))
+                    {
+                        if (func(u3) > func(u2))
+                        {
+                            u1 = u2;
+                            u2 = w;
+                        }
+                        else if (func(u1) > func(u2))
+                        {
+                            u3 = w;
+                        }
+                    }
                 }
+
+                if (w == u2)
+                {
+                    if (func(u2 - delta) < func(u2) && func(u2 - delta) < func(u1))
+                    {
+                        u2 = u2 - delta;
+                    }
+                }
+                counter++;
             }
-
-            if (w == u2)
-            {
-
-            }
-
-
-
-
+            uRes = u2;
+            jRes = func(uRes);            
         }
-
     }
 }
